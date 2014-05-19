ca8134bc23c8c2320b0898ef4aae6610698e3505
Tux <tux@netz39.de>
2014-05-19 10:40:13 +0200

    Check for space status before tweeting

--- SpaceAPI/www/cgi-bin/close.sh
+++ SpaceAPI/www/cgi-bin/close.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Includes (see http://stackoverflow.com/a/12694189)
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/apifunctions.sh"
+
+
 # HTTP-Headers
 echo "Content-Type: text/plain"
 echo "Access-Control-Allow-Origin: *"
 echo
+
 
 ./color.sh redblink
 ./spaceapi.sh false
@@ -11,12 +18,22 @@ echo
 ln -fs closed.png ../state.png
 touch ../closed.png
 
-./tweet.sh "Das @netz39 beendet die #Spacetime! ($(date "+%Y-%m-%d %H:%M"))"
+# only if open
+isopen=$(space_is_open)
+if [ "$isopen" == "true" ]; then
 
-echo
-echo 
-echo "Ampel wird in 5 Minuten ausgeschaltet."
+  ./tweet.sh "Das @netz39 beendet die #Spacetime! ($(date "+%Y-%m-%d %H:%M"))"
 
-echo "/var/www/cgi-bin/color.sh none" | at now + 5 minutes
+  echo
+  echo 
+  echo "Ampel wird in 5 Minuten ausgeschaltet."
+
+  echo "/var/www/cgi-bin/color.sh none" | at now + 5 minutes
+
+else
+
+  echo "Already closed!"
+
+fi
 
 ./color.sh red
--- SpaceAPI/www/cgi-bin/open.sh
+++ SpaceAPI/www/cgi-bin/open.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# Includes (see http://stackoverflow.com/a/12694189)
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+source "$DIR/apifunctions.sh"
+
+
 # HTTP-Headers
 echo "Content-Type: text/plain"
 echo "Access-Control-Allow-Origin: *"
 echo
+
 
 ./color.sh greenblink
 ./spaceapi.sh true
@@ -11,8 +18,17 @@ echo
 ln -fs open.png ../state.png
 touch ../open.png
 
-./tweet.sh "#Spacetime im @netz39! ($(date "+%Y-%m-%d %H:%M"))"
+# only if not already open
+isopen=$(space_is_open)
+if [ "$isopen" != "true" ]; then
 
+  ./tweet.sh "#Spacetime im @netz39! ($(date "+%Y-%m-%d %H:%M"))"
+
+else
+
+  echo "Already open!"
+
+fi
+  
 echo "/var/www/cgi-bin/color.sh green" | at now + 5 minutes
-
 ./color.sh green
