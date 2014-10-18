9a333bb3c4c4cd3750cead43d306e3ccaba772ea
Tux <tux@netz39.de>
2014-10-18 22:36:02 +0200

    color.sh mit i3c-Client-Aufrufen

--- SpaceAPI/www/cgi-bin/color.sh
+++ SpaceAPI/www/cgi-bin/color.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-gpio load i2c
+PEER=i3c.wittgenstein@platon
+DEVICE=0x20
+#gpio load i2c
 
 case "$1" in
   none)
-    i2cset -y 1 0x20 0x20
+#    i2cset -y 1 0x20 0x20
+    ./i3c_cli $PEER $DEVICE 0x2 0x0 
   ;;
   
   red)
-    i2cset -y 1 0x20 0x21
+#    i2cset -y 1 0x20 0x21
+    ./i3c_cli $PEER $DEVICE 0x2 0x1 
   ;;
   
   green)
-    i2cset -y 1 0x20 0x22
+#    i2cset -y 1 0x20 0x22
+    ./i3c_cli $PEER $DEVICE 0x2 0x2 
   ;;
   
   redblink)
-    i2cset -y 1 0x20 0x29
+#    i2cset -y 1 0x20 0xa9
+    ./i3c_cli $PEER $DEVICE 0x2 0x9 
   ;;
   
   greenblink)
-    i2cset -y 1 0x20 0x2a
+#    i2cset -y 1 0x20 0xaa
+    ./i3c_cli $PEER $DEVICE 0x2 0xa 
   ;;
   
   *)
